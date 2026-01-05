f4791d3fa7700b65325ad2e96ad3689c020ff66c
Daisuke Tsukada <dsgarage@gmail.com>
2026-01-05 21:57:27 +0900

    Initial Commit

---
+++ ydevARAppTests/ydevARAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ydevARAppTests.swift
+//  ydevARAppTests
+//
+//  Created by Daisuke Tsukada on 2026/01/05.
+//
+
+import Testing
+@testable import ydevARApp
+
+struct ydevARAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
